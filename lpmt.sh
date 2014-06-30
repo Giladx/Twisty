28af5f1f67b0198d5e30d73065de4ffe02a5157e
Giladx <gilad.x@gmail.com>
2014-06-30 19:51:29 +0300

    adding files

---
+++ lpmt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+
+echo "start the display"
+ 
+cd ~/of_v0.8.1/apps/myApps/lpmt/bin && ./lpmt
---
+++ twisty.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+
+echo "Start the production :) motheafucker !!!"
+ 
+cd ~/develop/Twisty && pd-extended -noaudio -noadc remix_main.pd
