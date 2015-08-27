f2522b2bef129053032cbd8a4cd122919e630303
giladx <gilad.x@gmail.com>
2015-08-27 13:17:27 +0300

    gui changes

--- twisty.sh
+++ twisty.sh
@@ -2,6 +2,6 @@
 
 clear
 
-echo "Start the production :) motheafucker !!!"
- 
-cd ~/develop/Twisty && pd-extended -noaudio -noadc remix_main.pd
+echo "Start the production :)"
+
+cd ~/Twisty && pd-extended -noaudio -noadc remix_main.pd
